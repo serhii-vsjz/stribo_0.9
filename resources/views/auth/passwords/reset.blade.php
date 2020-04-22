875431cf5e4d5477db276177cdc5226b1cb1937b
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2020-04-22 12:55:59 +0300

    update auth resourse

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -55,13 +55,7 @@
                         </button>
                     </div>
                 </div>
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Reset Password') }}
-                        </button>
-                    </div>
-                </div>
+
             </form>
         </div>
     </div>
