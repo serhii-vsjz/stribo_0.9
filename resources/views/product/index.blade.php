5781cdc17a5c1f6cfdb0d5e63d90d8b8abc7cc21
Serhii-Vasyliev-k <vasiliev.ck@gmail.com>
2020-05-07 20:09:32 +0300

    added product table

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -5,7 +5,7 @@
     @include('category._navigate')
 
 <div class="main__products">
-    <img class="image" src="{{ asset($currentCategory->image) }}">
+    <img class="image" src="{{ asset($currentCategory->image) }}" alt="">
     <h2>{{ $currentCategory->title }}</h2>
     <h3>{{ $currentCategory->vender }}</h3>
         <table class="table">
@@ -37,38 +37,47 @@
 
                 @endforeach
             @can('is_admin')
-                <div class="form_action">
-                    <form class="form" action="{{ route('product.create', ['category' => $product]) }}" method="GET">
-                        @csrf
-
-                        <button class="btn btn-edit" type="submit">Добавить</button>
-                    </form>
-
-                    <form class="form" action="{{ route('product.edit', ['product' => $product]) }}" method="GET">
-                        @csrf
-                        <button class="btn btn-edit" type="submit">Изменить</button>
-                    </form>
-
-                    <form class="form" action="{{ route('product.destroy', ['product' => $product]) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button  class="btn btn-delete" type="submit">Удалить</button>
-                    </form>
-                </div>
+                <tr>
+                    <td>{{ $product->title }}</td>
+                    <td>{{ $product->vendor }}</td>
+                    <td>22.50</td>
+                    <td>
+                        <form class="form">
+                            <input type="text" name="count">
+                        </form>
+                    </td>
+                    <td>
+                        <a class="link" href="{{ route('product.create', ['category' => $currentCategory])}}">
+                            <img class="picture" src="{{ asset('img/add.png') }}">
+                        </a>
+                    </td>
+                </tr>
             @endcan
+
 
         </table>
 
-    @can('is_admin')
-        <div class="card">
-            <a class="link" href="{{ route('product.create', ['category' => $currentCategory])}}">
+    {{--@can('is_admin')
+        <div class="form_action">
+            <form class="form" action="{{ route('product.create', ['category' => $product]) }}" method="GET">
+                @csrf
 
-                <img class="picture" src="{{ asset('img/add.png') }}">
-                <h2 class="title">Добавить</h2>
+                <button class="btn btn-edit" type="submit">Добавить</button>
+            </form>
 
-            </a>
+            <form class="form" action="{{ route('product.edit', ['product' => $product]) }}" method="GET">
+                @csrf
+                <button class="btn btn-edit" type="submit">Изменить</button>
+            </form>
+
+            <form class="form" action="{{ route('product.destroy', ['product' => $product]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button  class="btn btn-delete" type="submit">Удалить</button>
+            </form>
         </div>
-    @endcan
+    @endcan--}}
+
 </div>
 
 @endsection
